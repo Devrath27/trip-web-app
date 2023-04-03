a97a1044b30e260b7fbbfb582c8bfc0f62ed5e25
Parveen27 <parveen@argoid.com>
2023-04-03 23:36:25 +0530

    navigation bar is done

---
+++ frontend/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+  path: 'navigation', 
+  loadChildren: () => import('./navigation/navigation.module').then(m => m.NavigationModule),
+  
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ frontend/src/app/navigation/navigation.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Route, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.scss']
+})
+export class NavigationComponent implements OnInit{
+  hovered=true;
+  arr=[1,1,1,1,1,1,1,1];
+  constructor(private router: Router){
+
+  }
+  ngOnInit(): void {
+    // this.router.navigate(['/navigation'])
+  }
+ collaps(e:any,t:boolean)
+  {
+  this.hovered=t;
+  e.stopPropagation()
+  }
+
+}
