5adb45246c7f7839c4fd66c36c4303823183d066
vikash1012 <vikashbihari10122001@gmail.com>
2023-05-27 13:00:21 +0530

    remove unused import

--- Backend/src/main/java/com/trip/backend/BackendApplication.java
+++ Backend/src/main/java/com/trip/backend/BackendApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import(CorsConfig.class)
 public class BackendApplication {
 
 	public static void main(String[] args) {
